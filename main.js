acfb208f0a2032bab5040bdd366fb0d8716e5519
Ivo Woinarowski <ivowoinarowski@gmail.com>
2021-09-06 17:32:34 -0300

    Inicie el desafio 1

---
+++ main.js
@@ -0,0 +1,29 @@
+//Entrada de datos con prompt y parseInt
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+let edad = parseInt(prompt("Ingrese su edad"));
+
+alert ("Hola," + " " + nombre + " " + apellido + "\n" + edad + " años.");
+
+//Calculo de edad
+
+let añoActual = "2021";
+let añoNacimiento = parseInt(prompt("Ingrese su año de nacimiento"));
+
+let CalculoEdad = añoActual - añoNacimiento;
+
+alert ("Tu edad es: " + CalculoEdad);
+
+
+
+
+
+
+
+
+
+//let numero = parseInt(prompt ("Ingrese su numero de habitación"));
+
+//alert ("Su numero de habitación es: " + numero)
+
