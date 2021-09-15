44a8f09926410c58194ccddb57907be92fce8240
Ivo Woinarowski <ivowoinarowski@gmail.com>
2021-09-15 20:30:45 -0300

    .

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// JS
+let edadUsuario = 0;
+function solicitarEdad(){
+	let edadUsuario = parseInt(prompt("Ingresar su edad"));
+	console.log("Su edad es: " + edadUsuario);
+}
+ let promedio = edadUsuario
+
+
+// For
+let inicio = parseInt(prompt ("Ingrese el día actual"))
+let finDeMes = 30;
+
+for (let edadUsuario; edadUsuario != "ESC" ; inicio++) {
+	console.log ("Inicio vale: ", inicio);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function PromedioEdad{
+	let promedioEdadUsuario = edadUsuario
+}
+
+
+while (solicitarEdad !== "ESC") {
+		console.log("Su edad es: " + edadUsuario);
+		edadUsuario = parseInt(prompt("Ingresar su edad"));
+}
+
+let edad
