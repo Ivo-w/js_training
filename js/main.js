b53a6387a59df672cd3193546e3173b6e7832ced
Ivo Woinarowski <ivowoinarowski@gmail.com>
2021-11-09 16:44:58 -0300

    cart complete

--- js/main.js
+++ js/main.js
@@ -1,66 +1,142 @@
-
-/*class CrearUsuarios {
-	constructor (nombre, edad, dni) {
-		this.nombre = nombre;
-		this.edad = edad;
-		this.dni = dni;
-	}
-}
-const usuario1 = new CrearUsuarios (
-	prompt("Ingresa tu nombre"),
-	parseInt (prompt ("Ingresa tu edad")),
-	parseInt (prompt("Ingrese su número de DNI")),
-);
-
-console.log (usuario1);*/
-
-
-/*const URLGET = "https://jsonplaceholder.typicode.com/posts"
-
-$("body").append('<button class="btnGet" id="btn1">TRAER</button>');
-
-$("#btn1").click(() => { 
-    $.get(URLGET, function (response, estado, state) {
-          if(state.status === 200){
-            let misDatos = response;
-            for (const dato of misDatos) {
-              $("body").append(`<div>
-                                   <h3>${dato.title}</h3>
-                                   <p> ${dato.body}</p>
-                                  </div>`);
-            }  
-          } else {
-          	$("body").append('<h3>Error de dato</h3>');
-          }
-    });
-});*/
-
-const productosSeller = [{id: 1, nombre: 'A Promised Land. Barack Obama', precio: 2200, urlimg:"https://images-na.ssl-images-amazon.com/images/I/91K6nY47glL.jpg" },
+/*const productosSeller = [{id: 1, nombre: 'A Promised Land. Barack Obama', precio: 2200, urlimg:"https://images-na.ssl-images-amazon.com/images/I/91K6nY47glL.jpg" },
                          {id: 2, nombre: 'Too Much and Never Enough: How My Family', precio: 2000, urlimg: "https://images-na.ssl-images-amazon.com/images/I/8116vy-AXCL.jpg" },
                          {id: 3, nombre: 'Where the Crawdads Sing. Delia Owens', precio: 1250, urlimg: "https://images-na.ssl-images-amazon.com/images/I/91JSPXvOobL.jpg"},
                          {id: 4, nombre: 'La madre de Frankenstein', precio: 1100, urlimg: "https://contentv2.tap-commerce.com/cover/large/9789876706094_1.jpg?id_com=1113"},
-                        ];
+                        ];*/
 
     /*listarProductos (padre, data, callback)*/
-    for (const producto of productosSeller) {
+    /*for (const producto of productosSeller) {
 
         $('#producto_Book').append(`
             <div class="productoBook">
               <input value="${producto.id}" type="hidden" >
-              <img class="imgTamanio" src=${producto.urlimg}>
-              <h3 class="paddingProducto titleBook"> ${producto.nombre} </h3>
+              <img class="imgTamanio item-image" src=${producto.urlimg}>
+              <h3 class="paddingProducto titleBook item-title"> ${producto.nombre} </h3>
               <div class="botonFlex">
-               <h3 class="paddingProducto priceBook"> $${producto.precio} </h3>
+               <h3 class="paddingProducto priceBook item-price"> $${producto.precio} </h3>
                <div class ="flexBoton">
                <button type="button" class="styleBoton paddingBTN"><ion-icon class="iconCarrito" name="cart-outline"></ion-icon>COMPRAR</button>
-                </div>
+                </div> 
               </div>
             </div>
             `);
-        };
-    //$('.titleBook').prepend(`<img src="./assets/img/portadaFinal.png">`);
+        };*/
+/*--------------------------------*/
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+  addToCartButton.addEventListener('click', addToCartClicked);
+});
 
-    /*{id: 5, nombre: 'Midnight Sun', precio: 250},
-                         {id: 6, nombre: 'Untamed', precio: 250},
-                         {id: 7, nombre: 'El fin y otros inicios', precio: 250},
-                         {id: 8, nombre: 'La madre de Frankenstein ', precio: 250}*/
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+
+const shoppingCartItemsContainer = document.querySelector(
+  '.shoppingCartItemsContainer'
+);
+
+function addToCartClicked(event) {
+  const button = event.target;
+  const item = button.closest('.item');
+
+  const itemTitle = item.querySelector('.item-title').textContent;
+  const itemPrice = item.querySelector('.item-price').textContent;
+  const itemImage = item.querySelector('.item-image').src;
+
+  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+}
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+    'shoppingCartItemTitle'
+  );
+  for (let i = 0; i < elementsTitle.length; i++) {
+    if (elementsTitle[i].innerText === itemTitle) {
+      let elementQuantity = elementsTitle[
+        i
+      ].parentElement.parentElement.parentElement.querySelector(
+        '.shoppingCartItemQuantity'
+      );
+      elementQuantity.value++;
+      $('.toast').toast('show');
+      updateShoppingCartTotal();
+      return;
+    }
+  }
+
+  const shoppingCartRow = document.createElement('div');
+  const shoppingCartContent = `
+  <div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImage} class="shopping-cart-image imgTamanio ">
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>`;
+  shoppingCartRow.innerHTML = shoppingCartContent;
+  shoppingCartItemsContainer.append(shoppingCartRow);
+
+  shoppingCartRow
+    .querySelector('.buttonDelete')
+    .addEventListener('click', removeShoppingCartItem);
+
+  shoppingCartRow
+    .querySelector('.shoppingCartItemQuantity')
+    .addEventListener('change', quantityChanged);
+
+  updateShoppingCartTotal();
+}
+
+function updateShoppingCartTotal() {
+  let total = 0;
+  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+  shoppingCartItems.forEach((shoppingCartItem) => {
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+      '.shoppingCartItemPrice'
+    );
+    const shoppingCartItemPrice = Number(
+      shoppingCartItemPriceElement.textContent.replace('$', '')
+    );
+    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+      '.shoppingCartItemQuantity'
+    );
+    const shoppingCartItemQuantity = Number(
+      shoppingCartItemQuantityElement.value
+    );
+    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+  });
+  shoppingCartTotal.innerHTML = `${total.toFixed(2)}`;
+}
+
+function removeShoppingCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.closest('.shoppingCartItem').remove();
+  updateShoppingCartTotal();
+}
+
+function quantityChanged(event) {
+  const input = event.target;
+  input.value <= 0 ? (input.value = 1) : null;
+  updateShoppingCartTotal();
+}
+
+function comprarButtonClicked() {
+  shoppingCartItemsContainer.innerHTML = '';
+  updateShoppingCartTotal();
+}
